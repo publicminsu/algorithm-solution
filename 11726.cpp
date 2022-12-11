6995dd53d6a7391489f865065f6014fbbd32afbb
publicminsu@naver.com <publicminsu@naver.com>
2022-12-11 16:25:14 +0900

    221211

---
+++ 11726.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int arr[1001], n;
+    cin >> n;
+    arr[1] = 1, arr[2] = 2;
+    for (int i = 3; i <= n; ++i)
+    {
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
+    }
+    cout << arr[n];
+}
