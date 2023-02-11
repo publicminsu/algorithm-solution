76ceef71ab6244cca3041aa7736105d286e565c5
publicminsu <publicminsu@naver.com>
2023-02-11 15:11:31 +0900

    230211

--- 2357_최솟값과 최댓값.cpp
+++ 2357_최솟값과 최댓값.cpp
@@ -2,46 +2,36 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-typedef long long ll;
-vector<ll> numV, minTree, maxTree;
-ll minInit(int start, int end, int node)
+vector<int> numV;
+vector<pair<int, int>> tree;
+pair<int, int> init(int start, int end, int node)
 {
     if (start == end)
-        return minTree[node] = numV[start];
+        return tree[node] = {numV[start], numV[start]};
     int mid = (start + end) >> 1;
-    return minTree[node] = min(minInit(start, mid, node << 1), minInit(mid + 1, end, (node << 1) + 1));
+    pair<int, int> leftVal, rightVal;
+    leftVal = init(start, mid, node << 1);
+    rightVal = init(mid + 1, end, (node << 1) + 1);
+    return tree[node] = {min(leftVal.first, rightVal.first), max(leftVal.second, rightVal.second)};
 }
-ll maxInit(int start, int end, int node)
-{
-    if (start == end)
-        return maxTree[node] = numV[start];
-    int mid = (start + end) >> 1;
-    return maxTree[node] = max(maxInit(start, mid, node << 1), maxInit(mid + 1, end, (node << 1) + 1));
-}
-ll minFind(int start, int end, int node, int left, int right)
+pair<int, int> find(int start, int end, int node, int left, int right)
 {
     if (start > right || end < left)
-        return 1000000001;
+        return {1000000001, 0};
     if (start >= left && end <= right)
-        return minTree[node];
+        return tree[node];
     int mid = (start + end) >> 1;
-    return min(minFind(start, mid, node << 1, left, right), minFind(mid + 1, end, (node << 1) + 1, left, right));
-}
-ll maxFind(int start, int end, int node, int left, int right)
-{
-    if (start > right || end < left)
-        return 0;
-    if (start >= left && end <= right)
-        return maxTree[node];
-    int mid = (start + end) >> 1;
-    return max(maxFind(start, mid, node << 1, left, right), maxFind(mid + 1, end, (node << 1) + 1, left, right));
+    pair<int, int> leftVal, rightVal;
+    leftVal = find(start, mid, node << 1, left, right);
+    rightVal = find(mid + 1, end, (node << 1) + 1, left, right);
+    return {min(leftVal.first, rightVal.first), max(leftVal.second, rightVal.second)};
 }
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0);
     int N, M;
     cin >> N >> M;
-    minTree = maxTree = vector<ll>((N << 2));
+    tree = vector<pair<int, int>>((N << 2));
     numV.push_back(0);
     for (int i = 0; i < N; ++i)
     {
@@ -49,12 +39,12 @@ int main()
         cin >> num;
         numV.push_back(num);
     }
-    minInit(1, N, 1);
-    maxInit(1, N, 1);
+    init(1, N, 1);
     for (int i = 0; i < M; ++i)
     {
         int l, r;
         cin >> l >> r;
-        cout << minFind(1, N, 1, l, r) << " " << maxFind(1, N, 1, l, r) << "\n";
+        auto cur = find(1, N, 1, l, r);
+        cout << cur.first << " " << cur.second << "\n";
     }
 }
