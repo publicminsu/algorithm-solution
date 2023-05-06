fd1075f2ffe6744d0d2bb6a3a41ba2636e7cd765
publicminsu <publicminsu@naver.com>
2023-05-06 23:57:41 +0900

    230506

---
+++ 15656_N과 M (7).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v, arr;
+int N, M;
+void dfs(int depth)
+{
+    if (depth == M)
+    {
+        for (int i : arr)
+            cout << i << " ";
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < N; ++i)
+    {
+        arr[depth] = v[i];
+        dfs(depth + 1);
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N >> M;
+    v = vector<int>(N);
+    arr = vector<int>(M);
+    for (int i = 0; i < N; ++i)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+    dfs(0);
+    return 0;
+}
