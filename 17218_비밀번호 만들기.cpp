ee7a9936deb771a73330fce233ff825ef1454ab7
publicminsu <publicminsu@naver.com>
2023-05-09 13:11:30 +0900

    230509

--- 17218_비밀번호 만들기.cpp
+++ 17218_비밀번호 만들기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    string word1, word2, ret;
+    string word1, word2;
     cin >> word1 >> word2;
     vector<vector<string>> dp(word1.length() + 1, vector<string>(word2.length() + 1));
     for (int i = 0; i < word1.length(); ++i)
