dd511b79e9befdd1254ab98b830c0888e527f717
publicminsu <publicminsu@naver.com>
2023-09-03 23:30:11 +0900

    230903

---
+++ 10775_공항.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> parent;
+int G, P, g, cnt;
+int find(int cur)
+{
+    if (parent[cur])
+        return parent[cur] = find(parent[cur] - 1);
+    else
+    {
+        if (cur == 0)
+        {
+            cout << cnt;
+            exit(0);
+        }
+        return parent[cur] = cur;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> G >> P;
+    parent = vector<int>(G + 1);
+    while (P--)
+    {
+        cin >> g;
+        parent[g] = find(g);
+        ++cnt;
+    }
+    cout << cnt;
+    return 0;
+}
