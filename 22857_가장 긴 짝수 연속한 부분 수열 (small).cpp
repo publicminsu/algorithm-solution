af903f50770341adaaa1be4917b3875dc260b02f
publicminsu <publicminsu@naver.com>
2023-10-03 17:10:15 +0900

    231003

--- 22857_가장 긴 짝수 연속한 부분 수열 (small).cpp
+++ 22857_가장 긴 짝수 연속한 부분 수열 (small).cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int N, K, ret;
+int N, K, ret, num;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> N >> K;
     vector<vector<int>> dp(K + 1, vector<int>(N));
-    vector<int> S(N);
-    for (int i = 0; i < N; ++i)
-        cin >> S[i];
-    dp[0][0] = !(S[0] % 2);
+    cin >> num;
+    dp[0][0] = !(num % 2);
     ret = max(ret, dp[0][0]);
     for (int i = 1; i < N; ++i)
-        if (S[i] % 2) // 짝수인가
+    {
+        cin >> num;
+        if (num % 2) // 짝수인가
             for (int j = 1; j <= K; ++j)
                 dp[j][i] = dp[j - 1][i - 1];
         else
@@ -22,6 +22,7 @@ int main()
                 dp[j][i] = dp[j][i - 1] + 1;
                 ret = max(ret, dp[j][i]);
             }
+    }
     cout << ret;
     return 0;
 }
