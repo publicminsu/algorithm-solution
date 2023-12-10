1ffce430f68970866125876a792573090ebcfd69
publicminsu <publicminsu@naver.com>
2023-12-11 02:01:04 +0900

    231211

---
+++ baekjoon/10866_덱.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int N, X;
+    string cmd;
+    deque<int> dq;
+    cin >> N;
+    while (N--)
+    {
+        cin >> cmd;
+        if (cmd == "push_front")
+        {
+            cin >> X;
+            dq.push_front(X);
+        }
+        else if (cmd == "push_back")
+        {
+            cin >> X;
+            dq.push_back(X);
+        }
+        else if (cmd == "pop_front")
+        {
+            if (dq.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << dq.front() << "\n";
+                dq.pop_front();
+            }
+        }
+        else if (cmd == "pop_back")
+        {
+            if (dq.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << dq.back() << "\n";
+                dq.pop_back();
+            }
+        }
+        else if (cmd == "size")
+        {
+            cout << dq.size() << "\n";
+        }
+        else if (cmd == "empty")
+        {
+            cout << dq.empty() << "\n";
+        }
+        else if (cmd == "front")
+        {
+            if (dq.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << dq.front() << "\n";
+            }
+        }
+        else
+        {
+            if (dq.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << dq.back() << "\n";
+            }
+        }
+    }
+    return 0;
+}
