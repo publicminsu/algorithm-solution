bcef164d238a638c0d66304aa9d0638ece7534cb
publicminsu <publicminsu@naver.com>
2024-01-23 21:22:39 +0900

    240123

---
+++ baekjoon/1427_소트인사이드.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string num;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> num;
+    sort(num.begin(), num.end(), greater<char>());
+    cout << num;
+    return 0;
+}
