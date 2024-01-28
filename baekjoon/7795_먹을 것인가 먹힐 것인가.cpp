34675eacfb277cd5741064e9fbef74aab2621314
publicminsu <publicminsu@naver.com>
2024-01-28 21:48:44 +0900

    240128

---
+++ baekjoon/7795_먹을 것인가 먹힐 것인가.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define SIZE 20000
+int A[SIZE], B[SIZE];
+int T, N, M, answer;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> T;
+    while (T--)
+    {
+        answer = 0;
+
+        cin >> N >> M;
+        for (int i = 0; i < N; ++i)
+        {
+            cin >> A[i];
+        }
+        for (int i = 0; i < M; ++i)
+        {
+            cin >> B[i];
+        }
+
+        sort(A, A + N, greater<int>());
+        sort(B, B + M);
+
+        for (int i = 0; i < N; ++i)
+        {
+            auto iter = lower_bound(B, B + M, A[i]); // A[i] 이상인 것 찾기
+            answer += (iter - B);
+        }
+
+        cout << answer << "\n";
+    }
+    return 0;
+}
