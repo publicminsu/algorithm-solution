477261a5f1cb3ec37a5be810bffd27a45267895c
publicminsu <publicminsu@naver.com>
2024-01-30 00:27:11 +0900

    240130

---
+++ baekjoon/31287_장난감 강아지.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+string str;
+int curY, curX, N, K;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N >> K;
+    cin >> str;
+
+    if (K > 10000)
+    {
+        K = 10000;
+    }
+    while (K--)
+    {
+        for (char c : str)
+        {
+            if (c == 'U')
+            {
+                ++curY;
+            }
+            else if (c == 'D')
+            {
+                --curY;
+            }
+            else if (c == 'L')
+            {
+                --curX;
+            }
+            else
+            {
+                ++curX;
+            }
+            if (curY == 0 && curX == 0)
+            {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
