1dbf2420a522f25fea5e2c94fedc998ee33a48e7
publicminsu <publicminsu@naver.com>
2024-03-11 01:17:26 +0900

    240311

---
+++ baekjoon/31561_시계탑.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+float M, answer;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> M;
+    cout << fixed;
+    cout.precision(1);
+
+    if (M > 30)
+    {
+        answer = 15 + (M - 30) * 3 / 2;
+    }
+    else
+    {
+        answer = M / 2;
+    }
+
+    cout << answer;
+    return 0;
+}
