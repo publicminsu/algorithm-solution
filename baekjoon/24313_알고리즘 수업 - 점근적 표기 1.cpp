17fd327b5e1ae7fafba7f13d892a676b76d4c9fb
publicminsu <publicminsu@naver.com>
2024-03-11 21:33:37 +0900

    240311

--- baekjoon/24313_알고리즘 수업 - 점근적 표기 1.cpp
+++ baekjoon/24313_알고리즘 수업 - 점근적 표기 1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-double a1, a0, c, n;
+int a1, a0, c, n;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> a1 >> a0 >> c >> n;
 
-    cout << ((a0 / (c - a1) <= n) && (a1 <= c));
+    cout << ((a1 * n + a0 <= n * c) && (a1 <= c));
     return 0;
 }
