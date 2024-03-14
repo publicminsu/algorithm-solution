7341550ddb35b6a037986d7213aed6ccdbcd8fa7
publicminsu <publicminsu@naver.com>
2024-03-14 22:11:17 +0900

    240314

--- baekjoon/14916_거스름돈.cpp
+++ baekjoon/14916_거스름돈.cpp
@@ -5,16 +5,17 @@ int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n;
+
+    if (!(n % 5))
+    {
+        answer = n / 5;
+    }
+
     while (n > 0)
     {
         if (!(n % 2))
         {
             answer = min(answer, n / 2 + cnt);
-        }
-
-        if (!(n % 5))
-        {
-            answer = min(answer, n / 5 + cnt);
         }
 
         ++cnt;
