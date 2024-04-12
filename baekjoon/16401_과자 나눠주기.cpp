bd1c7f4a276ca6fb0f07f321938081e7e60cb67b
publicminsu <publicminsu@naver.com>
2024-04-12 20:54:24 +0900

    240412

---
+++ baekjoon/16401_과자 나눠주기.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+int M, N, cnt;
+ll answer, l, r, m;
+vector<ll> L;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> M >> N;
+    L = vector<ll>(N);
+    for (ll &l : L)
+    {
+        cin >> l;
+    }
+    sort(L.begin(), L.end(), greater<int>());
+
+    r = L.front() << 1;
+
+    while (l <= r)
+    {
+        cnt = 0;
+        m = (l + r) >> 1;
+        if (!m)
+        {
+            break;
+        }
+
+        for (const ll &l : L)
+        {
+            cnt += l / m;
+
+            if (m > l || cnt > M)
+            {
+                break;
+            }
+        }
+
+        if (cnt >= M)
+        {
+            answer = m;
+            l = m + 1;
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
