a81932fdbf5e3a382b22d75992ad9b6cc31a8dd2
publicminsu <publicminsu@naver.com>
2024-05-20 02:23:41 +0900

    240520

---
+++ baekjoon/31844_창고지기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int robot, box, goal;
+    for (int i = 0; i < 10; ++i)
+    {
+        char c;
+        cin >> c;
+        if (c == '@')
+        {
+            robot = i;
+        }
+        else if (c == '#')
+        {
+            box = i;
+        }
+        else if (c == '!')
+        {
+            goal = i;
+        }
+    }
+    if ((robot < box && box < goal) || (robot > box && box > goal))
+    {
+        cout << abs(robot - goal) - 1;
+    }
+    else
+    {
+        cout << -1;
+    }
+    return 0;
+}
