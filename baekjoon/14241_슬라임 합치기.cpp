0a32911a8f659c825d10f08036018eec8b8a088b
publicminsu <publicminsu@naver.com>
2024-05-22 02:15:02 +0900

    240521

--- baekjoon/14241_슬라임 합치기.cpp
+++ baekjoon/14241_슬라임 합치기.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int N, answer;
+int N, answer, p, n;
 priority_queue<int> pq;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    cin >> N;
-    while (N--)
+    cin >> N >> p;
+    while (--N)
     {
-        int i;
-        cin >> i;
-        pq.push(i);
-    }
-
-    while (pq.size() != 1)
-    {
-        int first = pq.top();
-        pq.pop();
-        int second = pq.top();
-        pq.pop();
-
-        answer += first * second;
-        pq.push(first + second);
+        cin >> n;
+        answer += p * n;
+        p += n;
     }
 
     cout << answer;
