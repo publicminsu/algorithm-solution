3dee90bb3d2eb7d5a08ca1347fc3accfff8028ae
publicminsu <publicminsu@naver.com>
2024-06-23 21:25:28 +0900

    240623

---
+++ baekjoon/10869_사칙연산.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int A, B;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> A >> B;
+    cout << A + B << "\n"
+         << A - B << "\n"
+         << A * B << "\n"
+         << A / B << "\n"
+         << A % B;
+}
