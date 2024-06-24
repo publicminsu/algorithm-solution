f1a3f8b7da5d2e82889f4534495695f674d6e6e4
publicminsu <publicminsu@naver.com>
2024-06-24 22:11:41 +0900

    240624

--- baekjoon/10172_개.cpp
+++ baekjoon/10172_개.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    cout << "|\\_/|\n";
-    cout << "|q p|   /}\n";
-    cout << "( 0 )\"\"\"\\\n";
-    cout << "|\"^\"`    |\n";
-    cout << "||_/=\\\\__|";
+    cout << "|\\_/|\n"
+         << "|q p|   /}\n"
+         << "( 0 )\"\"\"\\\n"
+         << "|\"^\"`    |\n"
+         << "||_/=\\\\__|";
     return 0;
 }
