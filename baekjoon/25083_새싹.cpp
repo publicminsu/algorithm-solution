ed156f533ef526c904f42bce7eb8e13b88fa30bd
publicminsu <publicminsu@naver.com>
2024-07-02 01:41:42 +0900

    240627

---
+++ baekjoon/25083_새싹.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cout << "         ,r\'\"7\n"
+         << "r`-_   ,\'  ,/\n"
+         << " \\. \". L_r'\n"
+         << "   `~\\/\n"
+         << "      |\n"
+         << "      |";
+    return 0;
+}
