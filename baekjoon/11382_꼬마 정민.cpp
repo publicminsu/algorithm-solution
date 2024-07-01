159d66ea891ed4d045cbaa3038e6117def987a52
publicminsu <publicminsu@naver.com>
2024-07-02 01:41:56 +0900

    240629

---
+++ baekjoon/11382_꼬마 정민.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+long long A, B, C;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> A >> B >> C;
+    cout << A + B + C;
+    return 0;
+}
