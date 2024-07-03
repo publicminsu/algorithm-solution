1832b22c6d4cae15c7bae8dca0075f0d41b39310
publicminsu <publicminsu@naver.com>
2024-07-03 22:35:20 +0900

    240703

---
+++ baekjoon/1490_자리수로 나누기.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+string N;
+queue<string> q;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N;
+    q.push(N);
+
+    while (!q.empty())
+    {
+        string cur = q.front();
+        q.pop();
+
+        bool isFind = true;
+        for (int i = N.size(); i > 0; --i)
+        {
+            int j = (cur[i - 1] - '0');
+
+            if (j && (stoll(cur) % j))
+            {
+                isFind = false;
+            }
+        }
+
+        if (isFind)
+        {
+            cout << cur;
+            break;
+        }
+
+        for (char i = '0'; i <= '9'; ++i)
+        {
+            q.push(cur + i);
+        }
+    }
+    return 0;
+}
