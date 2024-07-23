cedc669867f3a4ca27930dc228b23af3efc71e04
publicminsu <publicminsu@naver.com>
2024-07-23 22:05:21 +0900

    240723

---
+++ baekjoon/6064_카잉 달력.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int T, M, N, x, y;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> T;
+    while (T--)
+    {
+        cin >> M >> N >> x >> y;
+
+        while (true)
+        {
+            if (x == y)
+            {
+                cout << x << "\n";
+                break;
+            }
+            else if (x > M * N || y > M * N)
+            {
+                cout << -1 << "\n";
+                break;
+            }
+
+            if (x < y)
+            {
+                x += M;
+            }
+            else
+            {
+                y += N;
+            }
+        }
+    }
+    return 0;
+}
