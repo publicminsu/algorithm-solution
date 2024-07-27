0d41825c9f0c439f332fdeaf2ed4f84cffdecbe1
publicminsu <publicminsu@naver.com>
2024-07-27 13:35:42 +0900

    240727

---
+++ baekjoon/10951_A+B - 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int A, B;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    while (cin >> A >> B)
+    {
+        cout << A + B << "\n";
+    }
+    return 0;
+}
