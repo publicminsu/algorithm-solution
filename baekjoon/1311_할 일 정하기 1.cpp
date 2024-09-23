359598f19f797ac934b144dc53793955c29edff3
publicminsu <publicminsu@naver.com>
2024-09-23 17:27:01 +0900

    240923

---
+++ baekjoon/1311_할 일 정하기 1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define INF 200001
+int N;
+vector<vector<int>> graph;
+vector<int> dp;
+
+void input()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N;
+    dp = vector<int>(1 << N, INF);
+    graph = vector<vector<int>>(N, vector<int>(N));
+    for (vector<int> &line : graph)
+    {
+        for (int &i : line)
+        {
+            cin >> i;
+        }
+    }
+}
+
+int count(int num)
+{
+    int cnt = 0;
+    while (num)
+    {
+        cnt += (num & 1);
+        num >>= 1;
+    }
+    return cnt;
+}
+
+void solve()
+{
+    dp[0] = 0;
+
+    for (int flag = 0; flag < 1 << N; flag++)
+    {
+        int people = count(flag);
+
+        for (int job = 0; job < N; job++)
+        {
+            if (flag & (1 << job))
+            {
+                continue;
+            }
+
+            int nextFlag = flag | (1 << job);
+
+            dp[nextFlag] = min(dp[nextFlag], dp[flag] + graph[people][job]);
+        }
+    }
+
+    cout << dp[(1 << N) - 1];
+}
+
+int main()
+{
+    input();
+    solve();
+    return 0;
+}
