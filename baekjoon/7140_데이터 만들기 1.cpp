a19c559a99d831f199c51bd016b9eeb214241b5e
publicminsu <publicminsu@naver.com>
2024-11-09 01:36:05 +0900

    241109

---
+++ baekjoon/7140_데이터 만들기 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cout << 101 << "\n";
+    for (int i = 0; i < 101; ++i)
+    {
+        cout << 0 << "\n";
+    }
+    cout << 1 << "\n"
+         << 0 << " " << 1;
+    return 0;
+}
