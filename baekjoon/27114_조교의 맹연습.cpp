653c739ba8df2879305ee721d6686d9aee24b560
publicminsu <publicminsu@naver.com>
2025-01-13 23:53:17 +0900

    250113

---
+++ baekjoon/27114_조교의 맹연습.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+struct node
+{
+    int dir, cnt, energy;
+};
+int dx[] = {3, 1, 2};
+int abc[3];
+int K, answer = 1000001;
+queue<node> q;
+bool isVisited[4][1000001];
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        cin >> abc[i];
+    }
+
+    cin >> K;
+
+    q.push({0, 0, 0});
+
+    while (!q.empty())
+    {
+        node cur = q.front();
+        q.pop();
+
+        bool &check = isVisited[cur.dir][cur.energy];
+
+        if (check)
+        {
+            continue;
+        }
+
+        check = true;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            int nextDir = (cur.dir + dx[i]) % 4;
+            int nextCnt = cur.cnt + 1;
+            int nextEnergy = cur.energy + abc[i];
+
+            if (nextEnergy >= K)
+            {
+                if (nextEnergy == K && nextDir == 0)
+                {
+                    answer = min(answer, nextCnt);
+                }
+                continue;
+            }
+
+            node next = {nextDir, nextCnt, nextEnergy};
+            q.push(next);
+        }
+    }
+
+    cout << (answer == 1000001 ? -1 : answer);
+    return 0;
+}
