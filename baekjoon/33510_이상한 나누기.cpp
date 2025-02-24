28cb012caa0caddd6f61cc81acf3045208c61643
publicminsu <publicminsu@naver.com>
2025-02-24 23:24:25 +0900

    250224

---
+++ baekjoon/33510_이상한 나누기.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int N, answer;
+char X[10000000];
+bool carry;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> X[i];
+    }
+
+    for (int i = N - 1; i >= 1; --i)
+    {
+        if (carry)
+        {
+            if (X[i] == '0')
+            {
+                ++answer;
+                carry = true;
+            }
+        }
+        else
+        {
+            if (X[i] == '1')
+            {
+                ++answer;
+                carry = true;
+            }
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
