215de8a84cb0a2373e758294430953930687a674
publicminsu <publicminsu@naver.com>
2025-04-08 02:04:28 +0900

    250408

---
+++ baekjoon/2442_별 찍기 - 5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N - i - 1; ++j)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < i * 2 + 1; ++j)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
