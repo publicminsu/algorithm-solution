33b97f3ff2db40b73ad968e66869bb65cfd4f04b
publicminsu <publicminsu@naver.com>
2025-09-20 02:37:04 +0900

    250920

--- baekjoon/11653_소인수분해.cpp
+++ baekjoon/11653_소인수분해.cpp
@@ -8,13 +8,21 @@ int main()
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> N;
 
-    for (int i = 2; N != 1; ++i)
+    int num = N;
+
+    for (int i = 2; i * i <= N && num != 1; ++i)
     {
-        while (N % i == 0)
+        while (num % i == 0)
         {
             cout << i << "\n";
-            N /= i;
+            num /= i;
         }
     }
+
+    if (num == 1)
+    {
+        return 0;
+    }
+    cout << num;
     return 0;
 }
