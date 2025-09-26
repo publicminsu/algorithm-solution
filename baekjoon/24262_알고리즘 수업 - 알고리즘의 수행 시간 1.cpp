d938680d15102e4f38aaeba1768d683b94fc9805
publicminsu <publicminsu@naver.com>
2025-09-27 03:21:24 +0900

    250927

---
+++ baekjoon/24262_알고리즘 수업 - 알고리즘의 수행 시간 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> n;
+    cout << 1 << "\n"
+         << 0;
+    return 0;
+}
