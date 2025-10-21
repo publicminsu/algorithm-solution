d3a135c6701167c15d20f6cfada12a622226b4cb
publicminsu <publicminsu@naver.com>
2025-10-22 02:08:47 +0900

    251022

--- baekjoon/2804_크로스워드 만들기.cpp
+++ baekjoon/2804_크로스워드 만들기.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+using pii = pair<int, int>;
+
 string A, B;
 char map[31][31];
 
@@ -10,7 +12,7 @@ void input()
     cin >> A >> B;
 }
 
-char FindChar()
+pii FindIndexes()
 {
     for (int i = 0; i < A.size(); ++i)
     {
@@ -18,39 +20,26 @@ char FindChar()
         {
             if (A[i] == B[j])
             {
-                return A[i];
+                return {i, j};
             }
         }
     }
 
-    return 'z';
+    return {0, 0};
 }
 
-int findIndex(const string &word, const char &c)
+void paintMap()
 {
-    for (int i = 0; i < word.size(); ++i)
-    {
-        if (word[i] == c)
-        {
-            return i;
-        }
-    }
-    return 0;
-}
-
-void paintMap(const char &c)
-{
-    int aIndex = findIndex(A, c);
-    int bIndex = findIndex(B, c);
+    pii indexes = FindIndexes();
 
     for (int i = 0; i < A.size(); ++i)
     {
-        map[bIndex][i] = A[i];
+        map[indexes.second][i] = A[i];
     }
 
     for (int i = 0; i < B.size(); ++i)
     {
-        map[i][aIndex] = B[i];
+        map[i][indexes.first] = B[i];
     }
 }
 
@@ -69,8 +58,7 @@ void print()
 int main()
 {
     input();
-    const char &c = FindChar();
-    paintMap(c);
+    paintMap();
     print();
     return 0;
 }
