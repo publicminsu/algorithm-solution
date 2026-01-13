b5a82cf3246be369322a5d3e86bde901a46e077f
publicminsu <publicminsu@naver.com>
2026-01-13 22:47:05 +0900

    260113

---
+++ baekjoon/1065_한수.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int N, cnt;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        int j = i;
+
+        bool isSeq = true;
+
+        int prevNum = j % 10;
+        j /= 10;
+        int num = j % 10;
+        j /= 10;
+
+        while (j)
+        {
+            int nextNum = j % 10;
+            j /= 10;
+
+            if (prevNum - num != num - nextNum)
+            {
+                isSeq = false;
+            }
+        }
+
+        if (isSeq)
+        {
+            ++cnt;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
